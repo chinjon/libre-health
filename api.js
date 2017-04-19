42cf82172a7e1cf67ede844985fd00608ac29ec9
Wesley Handy <wesleylhandy@gmail.com>
2017-04-18 21:14:45 -0400

    interaction list

--- api.js
+++ api.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 
 	function getRxcui(drug){
 	
-	
 		return new Promise((resolve, reject)=>{
 
 			axios.get(`https://rxnav.nlm.nih.gov/REST/drugs.json?name=${drug}`).then(response=>{
@@ -14,11 +13,15 @@ var fs = require('fs');
 
 				var SBD = conceptGroup.filter(e=> {return e.tty == 'SBD'});
 
-				var dosage = SBD[0].conceptProperties.find((e)=>e.name.includes(process.argv[3]));
-				console.log(dosage);
-				//we can filter SBD on dosage or brand-name
+				// var dosage = SBD[0].conceptProperties.find((e)=>e.name.includes(process.argv[3]));
+				// console.log(dosage);
+				// //we can filter SBD on dosage or brand-name
 
-				resolve(dosage.rxcui);
+				// resolve(dosage.rxcui);
+
+				console.log("RXCUI ", SBD[0].conceptProperties[0].rxcui);
+
+				resolve(SBD[0].conceptProperties[0].rxcui);
 				
 			}).catch((err)=>{if(err){reject(err)}});
 
@@ -46,6 +49,7 @@ var fs = require('fs');
 					return {name, severity, description};
 				});
 
+				compareDrugs(newArray);
 				fs.writeFileSync('interactionMapped.json', JSON.stringify(newArray), 'utf-8');
 			
 			}).catch((err)=>{if(err)console.log(err)});
@@ -53,4 +57,41 @@ var fs = require('fs');
 		});
 	}
 
-getInteractions(process.argv[2]);
+// const drugOne = process.argv[2];
+// const drugTwo = process.argv[3];
+
+function compareDrugs(arr) {
+
+	let interaction = arr.filter(e=> e.name.toLowerCase() === drugTwo);
+	console.log(interaction);
+}
+
+// getInteractions(drugOne);
+
+
+/// list of drugs option
+
+
+const drugs = process.argv.slice(2);
+
+var promiseArray =[];
+
+drugs.forEach((drug)=>{
+	promiseArray.push(getRxcui(drug));
+});
+
+Promise.all(promiseArray).then((data)=>{
+
+	fs.writeFileSync('rxcuis.json', JSON.stringify(data), 'utf-8');
+	
+	var sources = data.join('+');
+	console.log('Sources: ', sources);
+
+	axios.get(`https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=${sources}`)
+	.then((response)=>{
+
+		console.log(response.data);
+
+		fs.writeFileSync('interactionsList.json', JSON.stringify(response.data), 'utf-8');
+	})
+});
