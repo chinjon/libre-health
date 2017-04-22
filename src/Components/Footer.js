56f0c8676b63f4cc3fdd8f4d398d42e762257e3a
Jonathan Chin <jonathankchin@gmail.com>
2017-04-22 13:59:12 -0400

    working with forms - added Theory's pseudo code :sob:

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,19 +1,27 @@
 import React, {Component} from 'react';
 
-class Footer extends Component {
-    render() {
-        return (
-            <footer className="footer">
-                <div className="container">
-                    <div className="content has-text-centered">
-                        <p>
+import Radium from 'radium';
 
-                        </p>
-                    </div>
-                </div>
-            </footer>
-        )
-    }
+let styles = {
+  base: {
+    height: '30px',
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer" style={styles.base}>
+        <div className="container">
+          <div className="content has-text-centered">
+            <p>
+              <a href="#">Libre Health</a>
+            </p>
+          </div>
+        </div>
+      </footer>
+    )
+  }
 }
 
 export default Footer;
