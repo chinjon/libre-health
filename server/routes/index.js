fa669347e70eee628064e8c805a4e75d0dfb7be8
bklynate <nathanielcfa@gmail.com>
2017-04-24 00:37:15 -0400

    Use 'yarn start-dev' to get the server started

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var auth = require("../controllers/AuthController.js");
 
-// // restrict index for logged in user only
-// router.get('/', auth.home);
+router.get('*', (request,response) => {
+  response.render("../../public/index.html")
+});
 
-// // route to register page
-// router.get('/register', auth.register);
-
-// // route for register action
-// router.post('/register', auth.doRegister);
-
-// // route to login page
-// router.get('/login', auth.login);
-
-// // route for login action
-// router.post('/login', auth.doLogin);
-
-// // route for logout action
-// router.get('/logout', auth.logout);
-
-// module.exports = router;
+module.exports = router;
