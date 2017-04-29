762c919ae9cfce6eeb0ea7be9655209964a05c2d
chinjon <Jonathankchin@gmail.com>
2017-04-28 23:00:00 -0400

    working on integrating the backend ¯\_(ツ)_/¯ idk what i did

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,6 +2,30 @@ const passport = require('passport');
 const localStrategy = require('local-strategy');
 const db = require('./../models');
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser((user, done)=> {
+    console.log('serializeUser', user);
+    done(null, user);
+});
 
-})
+passport.deserializeUser((user, done)=>{
+    console.log('deserializeUser');
+    db.User.findById(id, (err, user)=>{
+        done(null, user);
+    });
+});
+
+passport.user(new localStrategy((username, password, done)=>{
+    console.log('username', username);
+    console.log('password', password);
+
+    db.User.findOne({username: username}).then((user)=>{
+        if(!user) {
+            return done(null, false, {message: 'Incorrect Credentials'})
+        }
+        var psswd = user ? user.password: '';
+        db.User.validPassword(password, psswd, (err, found) =>{
+            console.log('passport', err, user);
+            done(err, found ? user : false);
+        });
+    });
+}));
