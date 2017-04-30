e0002549d37ed90e17d1fe2c17605be4ead2f80a
bklynate <nathanielcfa@gmail.com>
2017-04-29 20:02:41 -0400

    broke the application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {browserHistory, Router} from 'react-router';
+import {browserHistory} from 'react-router';
 // import App from './App';
 
 import Routes from './routes'
 
 ReactDOM.render(
-  <Router history={browserHistory} />,
+  <Routes history={browserHistory} />,
   document.getElementById('root')
 );
