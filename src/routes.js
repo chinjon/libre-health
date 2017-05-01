ec3f09070c088dcf45234cda0194e62100c71bc7
Wesley Handy <wesleylhandy@gmail.com>
2017-04-30 22:19:45 -0400

    added back in app to routes, sending login function down to children

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {Router, Route} from 'react-router';
 
+import App from './App';
 import Landing from './Components/Landing/Landing';
 import Dashboard from './Components/Dashboard/DashboardBody';
 
+
+
 const Routes = (props) =>(
   <Router {...props}>
-    <Route path="/" component={Landing} />
-    <Route path="/Dashboard" component={Dashboard} />
-	{/*<Route path="/IntakeForm" component={IntakeForm} /> */}
+  	<Route component={App}>
+	    <Route path="/" component={Landing} />
+	    <Route path="/Dashboard" component={Dashboard} />
+		{/*<Route path="/IntakeForm" component={IntakeForm} /> */}
+	</Route>
   </Router>
 )
 
