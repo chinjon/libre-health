cc5ab588062e3607ec1ad194c08ec17ffa97af86
Wesley Handy <wesleylhandy@gmail.com>
2017-05-03 11:08:34 -0400

    dropdown form complete

---
+++ src/Components/Dashboard/MedsListDropDown.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+
+class MedsListDropDown extends Component {
+
+	constructor(props){
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.renderMedsList = this.renderMedsList.bind(this);
+		this.state = {
+			value: 'select'
+		}
+	}
+
+    onInputChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(e) {
+    	e.preventDefault();
+    	if(this.state.value !== 'select'){
+    		this.props.addMedication(this.props.medsList[value]);
+    	}
+    }
+
+    renderMedsList(){
+    	return (
+    		<select value={this.state.value} onChange={this.onInputChange}>
+    			<option value='select'>Select From DropDown</option>
+    			{this.props.medsList.map((med, i)=>
+    				<option key={i} value={i}>{med.name}</option>
+    			)}
+    		</select>
+    	)
+    	
+    }
+
+    render() {
+
+    	return (
+
+        	<form onSubmit={this.handleSubmit} className="field has-addons has-addons-centered">
+			    <p className="control">
+				    <span className="select is-small">
+	      				
+				        {this.renderMedsList}
+				   
+			        </span>
+			    </p>
+			    <p className="control">
+			        <button type='submit' className="button is-small">Add</button>
+			    </p>
+			</form>
+
+		)
+    }
+}
+
+export default MedsListSearchForm;
