54637181f788b5bc83c5cd85f0fc64ff8117d0fa
Jonathan Chin <jonathankchin@gmail.com>
2017-05-02 20:20:52 -0400

    started work on interactions

--- src/Components/Dashboard/MedsInteract.js
+++ src/Components/Dashboard/MedsInteract.js
@@ -6,14 +6,40 @@ class MedsInteract extends Component {
         return (
             <div className="box has-text-centered">
             <h5 className="title is-5">Medication Interactions</h5>
+                <div className="tabs is-medium">
+                    <ul>
+                        <li class="is-active"><a>Tylenol</a></li>
+                        <li><a>Xanax</a></li>
+                        <li><a>Cocaine</a></li>
+                        <li><a>Cronic</a></li>
+                    </ul>
+                </div>
                 <div className="media">
-                    <table className="table">
+                    <table className="table is-bordered is-striped is-narrow">
                         <thead>
                             <tr>
                                 <th>Medication</th>
-                                <th>Medication</th>
+                                <th>Severity</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            <tr className="is-selected">
+                                <td>Tylenol</td>
+                                <td>Crazy Bad</td>
+                            </tr>
+                            <tr>
+                                <td>Tylenol</td>
+                                <td>Crazy Bad</td>
+                            </tr>
+                            <tr>
+                                <td>Tylenol</td>
+                                <td>Crazy Bad</td>
+                            </tr>
+                            <tr>
+                                <td>Tylenol</td>
+                                <td>Crazy Bad</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
