af90308f370cd5bf184890b59526ad3f2daf6aa1
bklynate <nathanielcfa@gmail.com>
2017-05-05 00:43:03 -0400

    working video landing page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { browserHistory } from 'react-router';
+import { DefaultPlayer as Video } from 'react-html5video';
 
 import Navigation from './../Navigation';
 import Footer from './../Footer';
@@ -37,59 +38,33 @@ class Landing extends Component {
     return (
       <div>
         <Navigation newUser={this.props.newUser} login={this.props.login} isAuth={this.props.isAuth}/>
-          {/* <div className="homepage-hero-module">
-            <div className="video-container">
-              <div className="filter"></div>
-              <video autoplay loop className="fillWidth">
-                <source src={thisSrcMp4} type="video/mp4"/>Your browser does not support the video tag. I suggest you upgrade your browser.
-                <source src={thisSrcWebm} type="video/webm"/>Your browser does not support the video tag. I suggest you upgrade your browser.
-              </video>
-              <div className="poster hidden">
-                <img src={thisSrcJpeg}/>
+        {/* <section className="hero is-info is-large is-fullheight is-bold">
+          <div className="hero-body">
+            <div className="container"> */}
+              <div className="homepage-hero-module">
+                <div className="video-container">
+                  <h1 className="title is-1 has-text-centered landing-text">
+                    Libre Health
+                  </h1>
+                  <div className="filter"></div>
+                  <Video autoPlay loop
+                    poster={thisSrcJpeg}
+                    className="fillWidth">
+                    <source src={thisSrcMp4} type="video/mp4"/>
+                    <source src={thisSrcWebm} type="video/webm"/>
+                  </Video>
+                </div>
               </div>
-            </div>
-          </div> */}
-          <section className="hero is-fullheight is-dark">
-
-  <div className="hero-body">
-    <div className="container has-text-centered">
-      <div className="columns is-vcentered">
-        <div className="column is-5">
-          <figure className="image is-4by3">
-            <img src="https://images.unsplash.com/photo-1461669802687-84a1aee43a29?dpr=1&auto=format&crop=entropy&fit=crop&w=800&h=600&q=80" className="promo-img" alt="Description" />
-          </figure>
-        </div>
-        <div className="column is-6 is-offset-1">
-          <h1 className="title is-2">
-            Introducing Landing Page
-          </h1>
-          <h2 className="subtitle is-4">
-            It's time to say hello to bulma.
-          </h2>
-          <br />
-          <p className="control has-addons has-text-centered">
-            <input className="input is-expanded is-large" type="text" placeholder="Join the beta waitlist, you@example.org"/>
-            <a className="button is-large is-danger is-outlined">
-              Sign Up
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div className="hero-foot">
-    <div className="container">
-      <div className="tabs is-centered">
-        <ul>
-          {/* <li><a href="http://bulma.io">Made with bulma</a></li> */}
-          {/* <li><a>Copyright 2017 Libre Health</a></li> */}
-          {/* <li><a href="http://unsplash.com">Images via unsplash</a></li> */}
-        </ul>
-      </div>
-    </div>
-  </div>
-</section>
+              {/* <div className="columns">
+                <div className="column is-half is-offset-one-quarter">
+                  <h1 className="title is-1 has-text-centered">
+                    Libre Health
+                  </h1>
+                </div>
+              </div> */}
+            {/* </div>
+          </div>
+        </section> */}
         <Footer/>
       </div>
     )
@@ -99,25 +74,12 @@ class Landing extends Component {
 export default Landing;
 
 
-{/* <div className="homepage-hero-module">
+{/* <div class="homepage-hero-module">
   <div class="video-container">
     <div class="filter"></div>
     <video autoplay loop class="fillWidth">
       <source src="%PUBLIC_URL%/assets/video/MP4/Healthy-Life.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
       <source src="%PUBLIC_URL%/assets/video/WEBM/Healthy-Life.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
     </video>
-  </div>
-</div> */}
-
-
-{/* <div className="hero-body">
-  <div className="container">
-    <div className="columns">
-      <div className="column is-half is-offset-one-quarter">
-        <h1 className="title is-1 has-text-centered">
-          Libre Health
-        </h1>
-      </div>
-    </div>
   </div>
 </div> */}
